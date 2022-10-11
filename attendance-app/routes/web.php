ed346412dbc3cd4e4eeec4f25def5a7f69f2ee81
52063 <52063@etu.he2b.be>
2022-10-11 14:26:19 +0200

    Story 1 - Déploiement

--- attendance-app/routes/web.php
+++ attendance-app/routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/students');
 Route::get('/students', [StudentController::class, 'studentsView']);
+Route::get('/token', function (){return csrf_token();});
